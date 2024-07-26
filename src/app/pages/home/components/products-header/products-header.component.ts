e0bda8b82d6d48444c19087e0389dc57cff8addb
Kawaf <Chadi.Kawaf@dentsplysirona.com>
2024-07-26 11:39:48 +0200

    Created the products header component and initialized the filter categories.

--- src/app/pages/home/components/products-header/products-header.component.ts
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-products-header',
   templateUrl: './products-header.component.html',
 })
 export class ProductsHeaderComponent {
+@Output() columnsCountChange = new EventEmitter<number>();
+sort: string= 'desc';
+itemsShowCount = 12;
 
+
+
+onSortUpdated(newSort: string): void {
+this.sort = newSort
+}
+ 
+onItemUpdated(count: number): void{
+  this.itemsShowCount = count;
+}
+
+onColumnsUpdated(colsNum: number): void{
+this.columnsCountChange.emit(colsNum);
+}
 }
